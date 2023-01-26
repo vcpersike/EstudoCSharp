2fb7913d57d2240015e31f97682d9438ce6d118b
vcpersike <vcpersike@gmail.com>
2023-01-25 23:50:53 -0300

    first commit

---
+++ Bank/ContaCorrente.cs
@@ -0,0 +1,11 @@
+﻿namespace Bank
+{
+	public class ContaCorrente
+	{
+		public int numero_agencia;
+		public string conta;
+		public string titular;
+		public double saldo;
+	}
+}
+
---
+++ Bank/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Bank;
+
+internal class Programa
+{
+    private static void Main(string[] args)
+    {
+        ContaCorrente contaDoVictor = new ContaCorrente();
+        contaDoVictor.titular = "Victor";
+        contaDoVictor.numero_agencia = 15;
+        contaDoVictor.conta = "1010-x";
+        contaDoVictor.saldo = 100000;
+    }
+}
